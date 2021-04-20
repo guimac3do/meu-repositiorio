49e7b27d2635e2d2281408a39e25744093267e6a
Guilherme Macêdo <78856935+guimac3do@users.noreply.github.com>
2021-04-19 21:58:54 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -17,8 +17,16 @@ function initAcordeon(){
 }
 initAcordeon();
 
-const section = document.querySelectorAll('.js-section');
+const sections = document.querySelectorAll('.js-section');
+const windowMetade = window.innerHeight * 0.6;
 
+function animaScroll(){
+  sections.forEach((section => {
+    const sectionTop = section.getBoundingClientRect().top;
+    const isSectionVisible = (sectionTop - windowMetade) < 0;
+      if (isSectionVisible)
+        section.classList.add('ativo');
+  }))
+}
 
-
-window.addEventListener('scroll', initSection);
+window.addEventListener('scroll', animaScroll);
